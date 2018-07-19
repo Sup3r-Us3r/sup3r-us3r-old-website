9b04014382f28db0b3ea628ba2a76d86cc0cf716
Magno Tutor <pc.gam3rs.tuto@gmail.com>
2018-07-19 12:14:03 -0300

    site

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,35 +1,11 @@
-// Side Menu
-    const sideNav = document.querySelector('.sidenav');
-    M.Sidenav.init(sideNav, {});
+(function($){
+  $(function(){
 
-    // Slider
-    const slider = document.querySelector('.slider');
-    M.Slider.init(slider, {
-        indicators: false,
-        height: 500,
-        transition: 500,
-        interval: 6000
-      });
+    $('.dropdown-trigger').dropdown();
+    $(".dropdown-button").dropdown();
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
 
-    // Scrollspy
-    const ss = document.querySelectorAll('.scrollspy');
-    M.ScrollSpy.init(ss, {});
-
-    // Material Boxed
-    const mb = document.querySelectorAll('.materialboxed');
-    M.Materialbox.init(mb, {});
-
-    // Auto Complete
-    const ac = document.querySelector('.autocomplete');
-    M.Autocomplete.init(ac, {
-        data: {
-          "Aruba": null,
-          "Cancun Mexico": null,
-          "Hawaii": null,
-          "Florida": null,
-          "California": null,
-          "Jamaica": null,
-          "Europe": null,
-          "The Bahamas": null,
-        }
-      });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
